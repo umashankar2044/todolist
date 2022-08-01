f46ad25672cc80480aa52f29d3d5f8a3a2a33582
umashankar yadav <umashankaryadav2044@gmail.com>
2022-08-01 14:39:03 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus eveniet id ipsam, veritatis ipsa iure, nostrum
+                reprehenderit aut nam rerum aspernatur a incidunt, quaerat
+                laudantium quibusdam explicabo officiis magnam porro.</p>
+        </div>
+    )
+}
